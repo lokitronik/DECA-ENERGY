dfe7ab0b168f3db67a7ce90817c5cbc31f3389e7
lokitronik <90688498+lokitronik@users.noreply.github.com>
2026-09-02 11:02:41 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -3,11 +3,13 @@ export type Language = 'sv' | 'en';
 export interface ServiceItem {
   title: string;
   tag: string;
+  description: string;
 }
 
 export interface TranslationContent {
   slogan: string;
   about: string;
+  aboutText: string;
   services: string;
   contact: string;
 
